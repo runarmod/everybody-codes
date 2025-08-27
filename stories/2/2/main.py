95333f3d31af517267cb4d557ef19699774a6a98
Runar Saur Modahl <runarmod@gmail.com>
2025-08-28 01:06:07 +0200

    stories 2 quest 2 refactor

--- stories/2/2/main.py
+++ stories/2/2/main.py
@@ -1,6 +1,7 @@
 import time
 from collections import deque
 from itertools import cycle
+from typing import Literal
 
 test_inputs = [
     """GRBGGGBBBRRRRRRRR""",
@@ -18,11 +19,11 @@ def part1(test: bool = False):
     data = deque(get_input(1, test))
 
     for s, c in enumerate(cycle("RGB"), start=1):
-        # Bolt survives all ballons of the same color as it.
+        # Bolt survives all balloons of the same color as it.
         while len(data) and data[0] == c:
             data.popleft()
 
-        # If there is 0 ballons left, we are done.
+        # If there is 0 balloons left, we are done.
         # If there is 1 balloon left, it will be popped and we are done.
         if len(data) <= 1:
             return s
@@ -31,26 +32,12 @@ def part1(test: bool = False):
         data.popleft()
 
 
-def part2(test: bool = False):
-    data = deque(get_input(2, test) * 100)
-
-    for s, c in enumerate(cycle("RGB"), start=1):
-        # If the first balloon won't destroy the bolt,
-        # and there is a balloon at the exact opposite side,
-        # the opposite side is guaranteed to pop.
-        if data[0] == c and len(data) % 2 == 0:
-            del data[len(data) // 2]
-
-        # The first balloon will always pop.
-        data.popleft()
-
-        if len(data) == 0:
-            return s
-
-
-def part3(test: bool = False):
-    data = get_input(3, test) * 100_000
-    half1, half2 = deque(data[: len(data) // 2]), deque(data[len(data) // 2 :])
+def run_balloon_circle(data: list[Literal["R", "G", "B"]], repeat: int = 1) -> int:
+    repeated_data = data * repeat
+    (half1, half2) = (
+        deque(repeated_data[: len(repeated_data) // 2]),
+        deque(repeated_data[len(repeated_data) // 2 :]),
+    )
 
     for s, c in enumerate(cycle("RGB")):
         if len(half1) == 0 and len(half2) == 0:
@@ -59,19 +46,27 @@ def part3(test: bool = False):
         popped = half1.popleft()
 
         if (len(half1) + 1 + len(half2)) % 2 != 0:
-            # Bolt will go between ballons on other side,
-            # so only the very first ballon popped.
+            # Bolt will go between balloons on other side,
+            # so only the very first balloon popped.
             continue
 
         if popped == c:
-            # Made it through the first ballon.
+            # Made it through the first balloon.
             # Will pop the other side as well guaranteed.
             half2.popleft()
             continue
 
-        # Bolt didn't make it through the first ballon.
-        # Rotate the opposite-side-ballon to the first half.
+        # Bolt didn't make it through the first balloon.
+        # Rotate the opposite-side-balloon to the first half.
         half1.append(half2.popleft())
+
+
+def part2(test: bool = False):
+    return run_balloon_circle(get_input(2, test), 100)
+
+
+def part3(test: bool = False):
+    return run_balloon_circle(get_input(3, test), 100_000)
 
 
 def main():
